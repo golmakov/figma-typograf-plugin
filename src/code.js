73c04207f30fcd9f240922f470121e4efda0f0c0
Aleksander Golmakov <golmakov@skbkontur.ru>
2019-08-20 13:32:38 +0500

    Skip mixed styled text layers

--- src/code.js
+++ src/code.js
@@ -1,24 +1,19 @@
 import * as Typograf from '../node_modules/typograf/dist/typograf.min.js';
 const tp = new Typograf({locale: ['ru', 'en-US']});
 
-
-function getSelectedObjects() {
-    const selection = figma.currentPage.selection.filter(layer => layer.type === 'TEXT');
-    if (selection.length === 0) {
-        figma.closePlugin('Select at least 1 text layer.');
-    } else {
-        return selection;
-    }
-}
-
 // Фигма требует, чтобы перед любыми операциями с текстом, происходила асинхронная загрузка шрифта — https://www.figma.com/plugin-docs/api/TextNode/
 async function typografText(obj) {
-    if (obj.hasMissingFont != true) {
+    if (obj.hasMissingFont != true && obj.fontName != figma.mixed) {
         await figma.loadFontAsync(obj.fontName);
         obj.characters = tp.execute(obj.characters);
     }
 }
 
-getSelectedObjects().forEach(typografText);
+const selection = figma.currentPage.selection.filter(layer => layer.type === 'TEXT');
+if (selection.length === 0) {
+    figma.closePlugin('Select at least 1 text layer.');
+} else {
+    selection.forEach(typografText);
+}
 
 figma.closePlugin();
