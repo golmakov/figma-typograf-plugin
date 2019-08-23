6d3d52f5735db620fd33fa9f7fa4e151559770db
Aleksander Golmakov <golmakov@skbkontur.ru>
2019-08-23 12:43:25 +0500

    Added message when user selects mixed styled text

--- src/code.js
+++ src/code.js
@@ -1,17 +1,27 @@
 import * as Typograf from '../node_modules/typograf/dist/typograf.min.js';
 const tp = new Typograf({locale: ['ru', 'en-US']});
 
+function haveMixedStyle(obj) {
+    return obj.fontName === figma.mixed || obj.fontSize === figma.mixed || obj.textCase === figma.mixed || obj.textDecoration === figma.mixed || obj.letterSpacing === figma.mixed || obj.lineHeight === figma.mixed || obj.textStyleId === figma.mixed;
+}
+
 // Фигма требует, чтобы перед любыми операциями с текстом, происходила асинхронная загрузка шрифта — https://www.figma.com/plugin-docs/api/TextNode/
 async function typografText(obj) {
-    if (obj.hasMissingFont != true && obj.fontName != figma.mixed) {
-        await figma.loadFontAsync(obj.fontName);
-        obj.characters = tp.execute(obj.characters);
+    if (obj.hasMissingFont != true) {
+        if (haveMixedStyle(obj) === false) {
+            await figma.loadFontAsync(obj.fontName);
+            obj.characters = tp.execute(obj.characters);
+        } else {
+            figma.closePlugin("Mixed text styles can't be typografed");
+        }
+    } else {
+        figma.closePlugin("Text with missing fonts can't be typografed");
     }
 }
 
 const selection = figma.currentPage.selection.filter(layer => layer.type === 'TEXT');
 if (selection.length === 0) {
-    figma.closePlugin('Select at least 1 text layer.');
+    figma.closePlugin("Select at least 1 text layer");
 } else {
     selection.forEach(typografText);
 }
